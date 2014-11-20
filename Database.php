ee7ba2bc1acf6537706bb0dcde05d5909c5095e5
Dylan Miller <dmiller3199@gmail.com>
2014-11-20 08:54:31 -0800

    video 33

--- Database.php
+++ Database.php
@@ -11,6 +11,21 @@
             $this->username =$username;
             $this->password =$password;
             $this->database =$database;
+            
+            $this->connection = new mysqli($host, $username, $password);
+            if($this->connection->connect_error) {
+                die("Error: " . $this->connection->connect_error);
+}
+            $exists = $this->connection->select_db($database);
+            if($exists) {
+            $query = $this->connection->query("CREATE DATABASE $database");
+            if($query) {
+		echo "Successfully created database: " . $database;
+	}
+}
+else {
+	echo "Database already exists.";
+}
         }
         public function openConnection() {
             //to make this reference to the right connection i had to put $this->
