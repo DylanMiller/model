f64e23185100d8fe4ec1547023e4180dc42d2634
Dylan Miller <dmiller3199@gmail.com>
2014-11-21 09:15:10 -0800

    35 and 36

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 <?php
     require_once(__DIR__ . "/Database.php");
+    session_start();
     $path = "/blog/";
     
     $host = "localhost";
     $username = "root";
     $password = "root";
     $database = "blog_db";
+    
+    if(isset(_SESSION["connection"])) {
+    $connection = new Database($host, $username, $password, $database);
+    $_SESSION["connection"] = $connection;
+    }
